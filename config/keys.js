9143904a5a82c649acab8b094c895de90cc3ffa0
Ning <614443803@qq.com>
2019-02-20 17:47:22 +1100

    Connected to mongoDB

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://ning:ning123@cluster0-bqtus.mongodb.net/devconnector?retryWrites=true"
+};
--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+//Configure the database
+const db = require("./config/keys").mongoURI;
+
+//Connect mongodb through mongoose
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
 
 //set a simple route. Put the route in a different folder. Now it is for testing purpose
 app.get("/", (req, res) => res.send("Hello"));
